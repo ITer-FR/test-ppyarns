0d24b2e164f307b534b290c9f403d13a4f738940
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2021-03-19 14:49:08 +0100

    else-if statement created

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ document.getElementById('Shi').addEventListener('click', function(a) {
     localStorage.setItem('score', 0);
     console.log('shi')
         // 0 = Shi ; 1 = Fu ; 2 = Mi : Mi beats Fu beats Shi beats Mi.
-    localStorage.setItem('ennemyScore', Math.floor(Math.random() * 2))
-    )
+    localStorage.setItem('ennemyScore', Math.floor(Math.random() * 3))
+    
+    determinateWinner()
+
 })
 
 document.getElementById('Fu').addEventListener('click', function(a) {
@@ -22,10 +24,21 @@ document.getElementById('Mi').addEventListener('click', function(a) {
 
 })
 
-determinateWinner() {
-    if (condition) {
-        
-    } else {
-        
+function determinateWinner() {
+    const myScore = localStorage.getItem('score')
+    const ennemyScore = localStorage.getItem('ennemyScore')
+    if (myScore === 0 && ennemyScore ===  1) {
+        console.log('You lose : Shi vs Fu')
+        localStorage.clear()
+    } else if ( myScore === 0 && ennemyScore === 2 ) {
+        console.log('You win : Shi vs Mi')
+        localStorage.clear()
+    } else if ( myScore === 0 && ennemyScore === 0 ) {
+        console.log('equality, play another round !')
+        playAgainEquality();
     }
+}
+
+function playAgainEquality() {
+
 }
