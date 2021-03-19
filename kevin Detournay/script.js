3a64d95430fd5af98d798c5184b4d72c6ae11557
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-03-19 14:16:36 +0100

    initial commit

---
+++ kevin Detournay/script.js
@@ -0,0 +1,16 @@
+
+
+const app={
+    
+    
+    // init function
+    init: ()=>{
+
+        console.log('init !')
+        
+
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', app.init );
